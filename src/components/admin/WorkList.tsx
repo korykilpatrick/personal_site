951a2668d7ceebcf74f6d94aebfae6dfb13a7379
korykilpatrick <koryrkilpatrick@gmail.com>
2025-04-18 09:31:05 -0500

    one more types to centralize

--- src/components/admin/WorkList.tsx
+++ src/components/admin/WorkList.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import api from '../../services/api';
-import { WorkEntry } from '../../types/work'; // Import Work types
+import { WorkEntry } from '../../../types'; // Import Work types
 import { Button, Card } from '../common';
 import { Loading, ErrorDisplay, EmptyState } from '../ui';
 
